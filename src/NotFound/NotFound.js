4856cdc83ac19b878e2757f8d436812e4a785509
tauhid495 <tauhid495@gmail.com>
2022-11-18 21:11:01 +0600

    gi

--- src/NotFound/NotFound.js
+++ src/NotFound/NotFound.js
@@ -4,7 +4,7 @@ const NotFound = () => {
     return (
         <div className='min-h-screen'>
             <img src="https://i.ibb.co/pZf2K7j/404-error-page-templates.jpg" alt="" />
-            {/* nothing */}
+<p>Tauhid</p>
         </div>
     );
 };
